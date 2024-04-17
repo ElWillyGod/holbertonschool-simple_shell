954c76d207e603ea269d4d40f42dcd5cd6ebd22a
Matias Davezac <8675@holbertonstudents.com>
2024-04-17 21:18:27 +0000

    I'll branch next time. UWU

--- main.c
+++ main.c
@@ -6,8 +6,83 @@
 #define RESET "\033[0m"
 
 /**
+ * free_tokens - Frees the malloc'd tokens in the tokens list.
  *
+ * @tokens: Token list.
  */
+static void free_tokens(char **tokens)
+{
+	int i;
+
+	for (i = 0; tokens[i]; i++)
+		free(tokens[i]);
+}
+
+/**
+ * add_token_to_tokens - Adds a mallocd string cpy of token arg to tokens.
+ *
+ * @token: String to be malloc'd and inserted.
+ * @tokens_size: Size of list of tokens (tokens).
+ * @tokens: Malloc'd list of tokens.
+ *
+ * Return: Reallocated pointer of tokens.
+ */
+static char **add_token_to_tokens(const char *token, size_t *tokens_size,
+		char ***tokens)
+{
+	char *mallocd_token;
+	char **mallocator;
+
+	*tokens_size += 1;
+	mallocator = realloc(*tokens, sizeof(char **) * (*tokens_size));
+	if (!*mallocator)
+	{
+		/* handle malloc fail */
+	}
+	*tokens = mallocator;
+
+	if (token)
+	{
+		mallocd_token = _strdup(token);
+		if (!mallocd_token)
+		{
+			/* handle malloc fail */
+		}
+
+		*tokens[*tokens_size - 1] = mallocd_token;
+	}
+	else
+	{
+		*tokens[*tokens_size - 1] = NULL;
+	}
+
+	return (*tokens);
+}
+
+/**
+ * tokenize - Gets token from given string.
+ *
+ * @line: line to tokenize.
+ *
+ * Return: list of malloc'd tokens.
+ */
+static char **tokenize(char **line, char **tokens)
+{
+	size_t tokens_size;
+	char *token;
+
+	tokens_size = 0;
+	token = strok(*line, " \n\t");
+	while (token)
+	{
+		add_token_to_tokens(token, &tokens_size, &tokens);
+		token = strok(NULL, " \n\t");
+	}
+
+	add_token_to_tokens(NULL, &tokens_size, &tokens);
+
+	return (tokens);
+}
 
 /**
  * main - Main.
@@ -21,8 +96,8 @@ int main(int ac, char **av)
 {
 	int i;
 	char *line;
+	size_t line_size;
 	char **tokens;
-
 
 	/* WIP, Task 22 */
 	if (ac > 1)
@@ -32,23 +107,21 @@ int main(int ac, char **av)
 
 	line = malloc(120 * sizeof(char));
 	tokens = malloc(sizeof(char *));
-	args = malloc(sizeof(char *))
 	while (1)
 	{
 		printf("%s<<Shelloc Homes>> $ %s", RED, RESET);
 
 		/* WIP, Reads line using getline */
-		line = read_input(line);
+		line = get_line(&line, &size, stdin);
 
 		/* WIP, Tokenizes line into tokens */
-		tokens = tokenize(line);
+		tokens = tokenize(line, tokens);
 
-		/* WIP, Separates tokens if ;, then &&, then | (if any) */
-		args = separate(tokens);
-
-		/* WIP, Searches executables in builtins f pointer and path,
-		 * then executes if found, error if not. */
+		/* WIP */
 		result = shelloc(args);
+
+		/* Frees malloc'd strings from (tokens) */
+		free_tokens(tokens);
 
 		/* exit command and error handling */
 		if (result == -2)
@@ -59,10 +132,7 @@ int main(int ac, char **av)
 
 	free(line);
 	free(tokens);
-	free(args);
 	return (0);
 }
-
-
 
 
